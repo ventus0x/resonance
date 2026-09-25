f4d142117e2287b33863274e740a7c86dcd208c7
ventus0x <ventus0x@gmail.com>
2026-09-25 10:39:14 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/resonance/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
